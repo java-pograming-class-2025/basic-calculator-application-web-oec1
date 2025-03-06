ca2476ff36971f5f84067c134442cdad56cd4fad
web-oec1 <web_oec1@edu.kimura.ac.jp>
2025-03-06 01:45:28 +0000

    add ch01,02

---
+++ src/ch01/Main.java
@@ -0,0 +1,8 @@
+package ch01;
+
+public class Main {
+    public static void main(String[] args){
+        //ここにコードを追加してください
+        System.out.println("this is Main.java on chaptor01");
+    }
+}
---
+++ src/ch02/Main.java
@@ -0,0 +1,8 @@
+package ch02;
+
+public class Main {
+    public static void main(String[] args){
+        //ここにコードを追加してください
+        System.out.println("this is Main.java on chaptor02");
+    }
+}
